6e30dfa61c8b5df869b828ce72e4bcf9fb5bbc56
Ersulov05 <ersulov05@mail.ru>
2024-12-30 01:05:36 +0300

    EightPointResize Добавил ещё 4 точки, выделил под точку отдельный компонент

---
+++ src/views/WorkArea/Slide/Selection/resizePoint/ResizePoint.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from 'react'
+import { UseDragAndDropType } from '../../../../hooks/useDragAndDrop'
+
+type ResizePointProps = {
+    className?: string
+    dragPoint: UseDragAndDropType
+    saveTransformObjects: () => void
+}
+
+function ResizePoint({
+    className,
+    dragPoint,
+    saveTransformObjects,
+}: ResizePointProps) {
+
+    useEffect(() => {
+        if (!dragPoint.dragging && dragPoint.dragging !== null) {
+            saveTransformObjects()
+            dragPoint.position.x = 0
+            dragPoint.position.y = 0
+            // dragPoint.resetDrag()
+        }   
+    }, [dragPoint.dragging]);
+
+    return (
+        <div 
+            className={className}
+            onMouseDown={dragPoint.startDrag}>
+        </div>
+    )
+}
+
+export {
+    ResizePoint,
+}
